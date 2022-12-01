60511d4fa2fea6b48bf816c1d896748575c33df3
SodaCris <wang-jiaxun@outlook.com>
2022-12-01 11:18:02 +0800

    add tools

--- tools/db_connection.cpp
+++ tools/db_connection.cpp
@@ -1,15 +1,25 @@
 #include "db_connection.h"
+#include "config.h"
 
 QSqlQuery get_connection(){
   static QSqlDatabase db;
   static std::once_flag flag;
   std::call_once(flag, [&]() {
-    db=QSqlDatabase::addDatabase("QDM");
+    if(use_dameng){
+db=QSqlDatabase::addDatabase("QDM");
     db.setHostName("127.0.0.1");
     db.setPort(5236);
     db.setDatabaseName("DAMENG");
     db.setUserName("SYSDBA");
     db.setPassword("SYSDBA");
+    }else{
+    db = QSqlDatabase::addDatabase("QMYSQL");
+  db.setHostName("127.0.0.1");
+  db.setPort(3306);
+  db.setDatabaseName("test_insert");
+  db.setUserName("root");
+  db.setPassword("");
+   }
     if (!db.open()) {
       qDebug() << "failed: " << db.lastError().text().toLatin1();
       std::terminate();
